4137a742d651f78a8c08e538a96437caf6f66e63
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-11-13 23:34:57 -0500

    Extracted category select to it's own component

---
+++ src/Components/WishlistView/WishlistItem/WishlistItem.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+
+import WantedItem from "../WantedItem";
+import ItemPrices from "./../../../data/ItemPrices";
+import CategorySelect from "./CategorySelect";
+
+function WishlistItem(props) {
+  const [name, setName] = useState(props.name || "ITEM NAME");
+  const [category, setCategory] = useState(props.category || "Uncommon Outfit");
+  const [price, setPrice] = useState(props.price || "0");
+
+  const itemRef = React.createRef();
+
+  const setBackground = className => {
+    // All contrails and music packs are rare only
+    className = ["contrail", "music"].includes(className) ? "rare" : className;
+
+    itemRef.current.classList = "wishlist_item";
+    itemRef.current.classList.add(className);
+  };
+
+  const setDefaultPrice = category => {
+    let defaultPrice = 0;
+    category = category.toLowerCase();
+
+    for (const item of ItemPrices) {
+      if (item.category === category) defaultPrice = item.cost;
+    }
+
+    setPrice(defaultPrice);
+  };
+
+  const updateItem = (name, category, price) => {
+    const updatedItem = new WantedItem(name, category, price);
+
+    props.onChange(props.position, updatedItem);
+    setName(name);
+    setCategory(category);
+    setPrice(price);
+  };
+
+  const updateItemProperties = e => {
+    let rarity = e.target.value.split(" ")[0].toLowerCase();
+
+    setBackground(rarity);
+    setDefaultPrice(e.target.value.toLowerCase());
+  };
+
+  useEffect(() => {
+    let rarity = category.split(" ")[0].toLowerCase();
+
+    setBackground(rarity);
+    setDefaultPrice(category);
+  });
+
+  return (
+    <div className="wishlist_item" ref={itemRef}>
+      <input
+        type="text"
+        className="wishlist_input item_name_input"
+        defaultValue={props.name}
+        onChange={e => updateItem(e.target.value, category, price)}
+      />
+      <CategorySelect
+        category={category}
+        onChange={e => {
+          setDefaultPrice(e.target.value);
+          updateItem(name, e.target.value, price);
+        }}
+      />
+      <input
+        type="text"
+        className="wishlist_input item_cost_input"
+        value={price}
+        onChange={e => updateItem(name, category, e.target.value)}
+      />
+      <div
+        className="wishlist_delete"
+        onClick={() => props.removeItem(props.position)}
+      >
+        x
+      </div>
+      <div className="wishlist_handler">=</div>
+    </div>
+  );
+}
+
+export default WishlistItem;
--- src/Components/WishlistView/WishlistView.js
+++ src/Components/WishlistView/WishlistView.js
@@ -1,61 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import WishlistItem from './WishlistItem';
-import WantedItem from './WantedItem';
+import React, { useState, useEffect } from "react";
+import WishlistItem from "./WishlistItem/WishlistItem";
+import WantedItem from "./WantedItem";
 
-import './WishlistView.scss';
+import "./WishlistView.scss";
 
 function WishlistView() {
-    const storedWishlist = JSON.parse(localStorage.getItem('wishlist'));
-    const [wishlist, setWishlist] = useState(storedWishlist || []);
+  const storedWishlist = JSON.parse(localStorage.getItem("wishlist"));
+  const [wishlist, setWishlist] = useState(storedWishlist || []);
 
-    const addNewItem = () => {
-        setWishlist([
-            ...wishlist,
-            new WantedItem('TEXT', 'Uncommon Outfit', '800')
-        ]);
-    };
+  const addNewItem = () => {
+    setWishlist([
+      ...wishlist,
+      new WantedItem("TEXT", "Uncommon Outfit", "800")
+    ]);
+  };
 
-    const removeItem = position => {
-        const updatedWishlist = [...wishlist];
-        updatedWishlist.splice(position, 1);
+  const removeItem = position => {
+    const updatedWishlist = [...wishlist];
+    updatedWishlist.splice(position, 1);
 
-        setWishlist(updatedWishlist);
-    };
+    setWishlist(updatedWishlist);
+  };
 
-    const updateItem = (position, item) => {
-        const updatedWishlist = [...wishlist];
-        updatedWishlist[position] = item;
+  const updateItem = (position, item) => {
+    const updatedWishlist = [...wishlist];
+    updatedWishlist[position] = item;
 
-        setWishlist(updatedWishlist);
-    };
+    setWishlist(updatedWishlist);
+  };
 
-    useEffect(() => {
-        const wishlistString = JSON.stringify(wishlist);
+  useEffect(() => {
+    const wishlistString = JSON.stringify(wishlist);
 
-        localStorage.setItem('wishlist', wishlistString);
-    }, [wishlist]);
+    localStorage.setItem("wishlist", wishlistString);
+  }, [wishlist]);
 
-    return (
-        <div className='section'>
-            <h1>Wishlist</h1>
-            <div className='timeline_view card'>
-                {wishlist.map((item, index) => {
-                    return (
-                        <WishlistItem
-                            key={`${index}`}
-                            position={index}
-                            name={item.name}
-                            category={item.category}
-                            price={item.price}
-                            removeItem={removeItem}
-                            onChange={updateItem}
-                        />
-                    );
-                })}
-                <button onClick={addNewItem}>Add Item</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="section">
+      <h1>Wishlist</h1>
+      <div className="timeline_view card">
+        {wishlist.map((item, index) => {
+          return (
+            <WishlistItem
+              key={`${index}`}
+              position={index}
+              name={item.name}
+              category={item.category}
+              price={item.price}
+              removeItem={removeItem}
+              onChange={updateItem}
+            />
+          );
+        })}
+        <button onClick={addNewItem}>Add Item</button>
+      </div>
+    </div>
+  );
 }
 
 export default WishlistView;
