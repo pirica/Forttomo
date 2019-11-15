dfc09dc44f54000a986a7321f8d6d30d3b0a5271
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-11-15 16:44:59 -0500

    Added vbucks needed section to wishlist info

--- src/Components/WishlistView/WishlistView.js
+++ src/Components/WishlistView/WishlistView.js
@@ -6,12 +6,15 @@ import WishlistItem from './WishlistItem/WishlistItem';
 import WantedItem from './WantedItem';
 
 import WishlistContext from './../../context/WishlistContext';
+import InputContext from './../../context/InputContext';
 import './WishlistView.scss';
 
 function WishlistView() {
   const { wishlist, wishlistTotal, completionDate, setWishlist } = useContext(
     WishlistContext
   );
+  const { vbucks, missions, dailies } = useContext(InputContext);
+  const vbucksTotal = vbucks + missions + dailies;
 
   const addNewItem = () => {
     setWishlist([
@@ -56,9 +59,13 @@ function WishlistView() {
         </button>
         <div className='wishlist_data'>
           <div>Completion Date</div>
+          <div>Needed</div>
           <div>Total</div>
           <div className='completion_date'>{completionDate}</div>
-          <div className='wishlist_total'>{`Total: ${wishlistTotal}`}</div>
+          <div className='wishlist_needed_vbucks'>
+            {wishlistTotal - vbucksTotal > 0 ? wishlistTotal - vbucksTotal : 0}
+          </div>
+          <div className='wishlist_total'>{wishlistTotal}</div>
         </div>
       </div>
     </div>
