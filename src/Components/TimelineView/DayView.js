ebfb4ecbbddd9ce2b3059bb667c24580289aa096
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2019-11-15 16:26:48 -0500

    Implemented thursday class check to DayView

--- src/Components/TimelineView/DayView.js
+++ src/Components/TimelineView/DayView.js
@@ -2,14 +2,16 @@ import React from 'react';
 import LogView from './LogView';
 
 function DayView({ day }) {
-    return (
-        <div className='day_view'>
-            <div className='date'>{day.date}</div>
-            <div className='timeline_vbucks'>{day.vbucks}</div>
-            <div className='timeline_level'>Lv {day.level}</div>
-            <LogView logs={day.logs} />
-        </div>
-    );
+  const thursdayClass = day.date.split(',')[0] === 'Thursday' ? 'thursday' : '';
+
+  return (
+    <div className={`day_view ${thursdayClass}`}>
+      <div className='date'>{day.date}</div>
+      <div className='timeline_vbucks'>{day.vbucks}</div>
+      <div className='timeline_level'>Lv {day.level}</div>
+      <LogView logs={day.logs} />
+    </div>
+  );
 }
 
 export default DayView;
