97c5cc2cd873594b77ce052449faa1deceda09f3
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-03-10 01:36:25 -0400

    Fixed WishList item to update prices based on category change

--- src/Components/WishlistView/WishlistItem/WishlistItem.js
+++ src/Components/WishlistView/WishlistItem/WishlistItem.js
@@ -49,7 +49,7 @@ function WishlistItem(props) {
         type='text'
         className='wishlist_input item_name_input'
         defaultValue={props.name}
-        onBlur={e => updateItem(e.target.value, category, price)}
+        onChange={e => updateItem(e.target.value, category, price)}
       />
       <CategorySelect
         category={category}
@@ -60,8 +60,8 @@ function WishlistItem(props) {
       <input
         type='text'
         className='wishlist_input item_cost_input'
-        defaultValue={price}
-        onBlur={e => updateItem(name, category, e.target.value)}
+        value={price}
+        onChange={e => updateItem(name, category, e.target.value)}
       />
       <div
         className='wishlist_delete'
