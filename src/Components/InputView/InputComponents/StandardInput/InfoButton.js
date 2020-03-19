424778902bbabe29848f9d3f05779b24cde7835c
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-03-18 21:17:50 -0400

    Changed the character for the info buttton

--- src/Components/InputView/InputComponents/StandardInput/InfoButton.js
+++ src/Components/InputView/InputComponents/StandardInput/InfoButton.js
@@ -9,7 +9,7 @@ function InfoButton({ message }) {
       className='info_button_section'
       onClick={() => setMessageOpen(!messageOpen)}
     >
-      <div className='info_button'>?</div>
+      <div className='info_button'>i</div>
       <div
         className={`message_box ${animationClass}`}
         onClick={() => setMessageOpen(!messageOpen)}
