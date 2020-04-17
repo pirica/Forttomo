eb06d9649acd1dcf952570ad5b5716a6d33bacd0
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-04-16 21:35:47 -0400

    Implemented firebase functions

---
+++ firebase/functions/index.js
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+const SECRET_KEY = require('./secret_key.json');
+
+exports.secretKeyMatches = functions.https.onRequest(
+  async (request, response) => {
+    const userSecretKey = request.query.key;
+    const secretKey = SECRET_KEY.key;
+
+    response.send({ isCorrect: userSecretKey === secretKey });
+  }
+);
