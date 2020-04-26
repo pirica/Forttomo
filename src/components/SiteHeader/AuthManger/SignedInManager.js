8251cc9b3b4949bd4116a44264004a1493bb9573
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-04-26 03:44:00 -0400

    Implemented basic styling for account manager

--- src/components/SiteHeader/AuthManger/SignedInManager.js
+++ src/components/SiteHeader/AuthManger/SignedInManager.js
@@ -1,19 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 
 import AuthContext from '../../../context/AuthContext';
+import './AccountManager.scss';
 
-const SignedInManager = ({ username }) => {
-  const { logout } = useContext(AuthContext);
+const SignedInManager = () => {
+  const { username, logout } = useContext(AuthContext);
+  const dropdownRef = useRef();
 
   const handleSignOut = () => {
     logout();
   };
 
+  const toggleDropDown = () => {
+    dropdownRef.current.classList.toggle('droppeddown');
+  };
+
   return (
-    <div>
-      <div className='username_label'>{username}</div>
-      <div className='signout_link' onClick={handleSignOut}>
-        Sign Out
+    <div className='account_manager'>
+      <i className='fas fa-user-circle' />
+      <div className='username_label'>LMAOBoy</div>
+      <div className='dropdown_container'>
+        <i className='fas fa-sort-down' onClick={toggleDropDown} />
+        <div className='signin_dropdown' ref={dropdownRef}>
+          <div className='signout_link' onClick={handleSignOut}>
+            Sign Out
+          </div>
+        </div>
       </div>
     </div>
   );
