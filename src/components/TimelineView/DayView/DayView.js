36725d092d08f497b5ecaaf0d0c0b2a475c6ddab
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-04-30 21:42:04 -0400

    Refactored DayView

--- src/components/TimelineView/DayView/DayView.js
+++ src/components/TimelineView/DayView/DayView.js
@@ -1,32 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import UpdatableLabel from '../../Utility/UpdatableLabel';
 import BattlePassView from './BattlePassView';
 import './DayView.scss';
 
-function DayView({ day }) {
+const DayView = ({ day }) => {
+  const { date, vbucks, level, change, bpItems } = day;
   const thursdayClass = day.date.split(',')[0] === 'Thu' ? 'thursday' : '';
+  const containerClass = `timeline_row day_view ${thursdayClass}`;
 
   return (
-    <div className={`timeline_row day_view ${thursdayClass}`}>
-      <div className='date'>{day.date}</div>
+    <div className={containerClass}>
+      <div className='date'>{date}</div>
       <div className='timeline_label_wrapper'>
-        <UpdatableLabel className='timeline_vbucks'>
-          {day.vbucks}
-        </UpdatableLabel>
+        <UpdatableLabel className='timeline_vbucks'>{vbucks}</UpdatableLabel>
       </div>
       <div className='timeline_label_wrapper'>
-        <UpdatableLabel className='timeline_level'>{day.level}</UpdatableLabel>
+        <UpdatableLabel className='timeline_level'>{level}</UpdatableLabel>
       </div>
       <div className='timeline_label_wrapper'>
-        <UpdatableLabel className='timeline_change'>
-          {day.change}
-        </UpdatableLabel>
+        <UpdatableLabel className='timeline_change'>{change}</UpdatableLabel>
       </div>
-      {day.bpItems.length !== 0 && <BattlePassView items={day.bpItems} />}
+      {bpItems.length !== 0 && <BattlePassView items={bpItems} />}
       <div className='divider'></div>
     </div>
   );
-}
+};
+
+DayView.propTypes = {
+  day: PropTypes.shape({
+    date: PropTypes.string,
+    vbucks: PropTypes.number,
+    level: PropTypes.number,
+    change: PropTypes.string,
+    bpItems: PropTypes.array,
+  }).isRequired,
+};
 
 export default DayView;
