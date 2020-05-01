bc307b4fee49b7a07ed7787e947e997edf6f6506
Dmitriy Zuchko <dmitriyzuchko@gmail.com>
2020-04-30 21:25:25 -0400

    Refactored DelayedInput

--- src/components/Utility/DelayedInput.js
+++ src/components/Utility/DelayedInput.js
@@ -1,38 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const DelayedInput = React.forwardRef(
-  ({ type, value, onChange, className, delay }, ref) => {
-    const [internalValue, setInternalValue] = useState(value);
-    const typingTimeout = React.useRef(0);
-    delay = delay || 500;
+const DelayedInput = ({ type, value, onChange, className, delay }, ref) => {
+  const [internalValue, setInternalValue] = useState(value);
+  const typingTimeout = useRef();
+  delay = delay || 500;
 
-    const handleChange = e => {
-      const targetValue = e.target.value;
+  const handleChange = e => {
+    const targetValue = e.target.value;
 
-      if (typingTimeout.current) clearTimeout(typingTimeout.current);
+    if (typingTimeout.current) clearTimeout(typingTimeout.current);
 
-      typingTimeout.current = setTimeout(() => {
-        onChange(targetValue);
-      }, delay);
+    typingTimeout.current = setTimeout(() => {
+      onChange(targetValue);
+    }, delay);
 
-      setInternalValue(targetValue);
-    };
+    setInternalValue(targetValue);
+  };
 
-    // When the value changes externally, the state needs to update
-    useEffect(() => {
-      setInternalValue(value);
-    }, [value]);
+  // When the value changes externally, the state needs to update
+  useEffect(() => {
+    setInternalValue(value);
+  }, [value]);
 
-    return (
-      <input
-        className={className}
-        ref={ref}
-        type={type}
-        value={internalValue}
-        onChange={handleChange}
-      />
-    );
-  }
-);
+  return (
+    <input
+      className={className}
+      ref={ref}
+      type={type}
+      value={internalValue}
+      onChange={handleChange}
+    />
+  );
+};
 
-export default DelayedInput;
+export default React.forwardRef(DelayedInput);
